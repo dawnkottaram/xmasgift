581f43a891ff88ece97c33342bb9f7d03ae59e2b
winster <wtjose@gmail.com>
2023-12-26 19:01:17 +0000

    Update book.py

--- book.py
+++ book.py
@@ -20,9 +20,9 @@ def get_available_slots():
     if login_response.status_code != 200:
         print("No response for get slots.")
         return None
-    if is_json(slots_response.text) == False:
-        print("Response is not json.")
-        return None
+    # if is_json(slots_response.text) == False:
+    #    print("Response is not json.")
+    #    return None
     print(f"slots_response -> {slots_response.text}")
     slots_data = slots_response.json()
 
